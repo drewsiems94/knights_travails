cd6f0c93d9458e293738c49123bfe307c34985a8
Andrew Siems <siemsdrew@gmail.com>
2022-10-21 11:30:55 -0700

    Add class to mark the knight's position and parent node

--- knight.rb
+++ knight.rb
@@ -1,3 +1,11 @@
+
 class Knight
-  
+  MOVES = [[-1, -2], [-1, 2], [-2, -1], [-2, 1], [1, -2], [1, 2], [2, -1], [2, 1]]
+
+  attr_accessor :position, :parent
+
+  def initialize(position = nil)
+    @position = position
+    @parent = nil
+  end
 end
